bab04b2b42b5826be61a79171143cb8eb92394a6
Buckkeep <neeta.b+github@gmail.com>
2024-07-23 15:16:13 +0100

    Created a card component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import Footer from "./Footer";
-import Header from "./Header"
-import Food from "./Food";
+import Card from "./Card";
 
 function App() {
 
   return(
     <>
-      <Header/>
-      <Food/>
-      <Footer/> 
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </>
   );
 }
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card() {
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://www.drurys.co.uk/wp-content/uploads/2015/01/no-pic-avatar.png" alt="Profile picture" width="200"/>
+            <h2 className="card-title">Buckkeep</h2>
+            <p className="card-text">I am an iOS developer writing a website in React and I also like to play piano</p>
+        </div>
+    );
+}
+
+export default Card
