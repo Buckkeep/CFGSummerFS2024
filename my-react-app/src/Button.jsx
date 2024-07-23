e0a3f9b0c033ec0a4425845a35e7cda42b2a37ff
Buckkeep <neeta.b+github@gmail.com>
2024-07-23 16:09:42 +0100

    Inline CSS

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,16 @@
+
+function Button() {
+
+    const styles = {
+            backgroundColor: "hsl(200, 100%, 50%)",
+            color: "white",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",
+    }
+
+    return(<button style={styles}>Click Me</button>);
+}
+
+export default Button
--- my-react-app/src/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './button.module.css'
-
-function Button() {
-
-    return(<button className={styles.button}>Click Me</button>);
-}
-
-export default Button
