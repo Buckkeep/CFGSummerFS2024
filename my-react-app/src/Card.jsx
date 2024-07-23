8c0b70888415dc08d9a76c288fa9ab3ac81b1190
Buckkeep <neeta.b+github@gmail.com>
2024-07-23 15:51:10 +0100

    Updated favicon and avatar on cards.

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,9 +1,10 @@
+import wolfavatar from '/src/assets/wolfavatar.jpg'
 
 function Card() {
 
     return(
         <div className="card">
-            <img className="card-image" src="https://www.drurys.co.uk/wp-content/uploads/2015/01/no-pic-avatar.png" alt="Profile picture" width="200"/>
+            <img className="card-image" src={wolfavatar} alt="Profile picture" width="200"/>
             <h2 className="card-title">Buckkeep</h2>
             <p className="card-text">I am an iOS developer writing a website in React and I also like to play piano</p>
         </div>
