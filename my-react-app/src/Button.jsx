b23d61a1dafb39f52a6cd33c7c35c5eab84fba27
Buckkeep <neeta.b+github@gmail.com>
2024-07-24 16:34:25 +0100

    Events and reading the event object in the console, using that info to change the text on the button upon click.

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,19 +1,10 @@
 
 function Button() {
 
-    let count = 0;
-
-    const handleClick = (name) => {
-        if (count < 3) {
-            count ++;
-            console.log(`${name} you clicked me ${count} times`);
-        } else {
-            console.log(`${name}, stop clicking me!`);
-        }
-    }  
+    const handleClick = (e) => e.target.textContent = "OUCH!"
 
     return(
-        <button onClick={() => handleClick("Buckkeep")}>Click Me, bro 👌 </button>
+        <button onClick={(e) => handleClick(e)}>Click Me, bro 👌 </button>
     );
 }
 
