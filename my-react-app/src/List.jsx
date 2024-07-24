e7006a492fa839c7bf840e82f78f3e5ca3756791
Buckkeep <neeta.b+github@gmail.com>
2024-07-24 14:39:23 +0100

    Rendering a list of objects, adding a key and id, sorting alphabetically and numerically

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,11 +1,22 @@
 
 function List() {
 
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const fruits = [
+         {id: 1, name: "apple", calories: 95},
+         {id: 2, name: "orange", calories: 45}, 
+         {id: 3, name: "banana", calories: 105}, 
+         {id: 4, name: "coconut", calories: 159}, 
+         {id: 5, name: "pineapple", calories: 37}
+        ];
 
-    // fruits.sort();
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); // Sort alphabetically
+    // fruits.sort((a,b) => b.name.localeCompare(a.name));  // Sort reverse alphabetically
+    // fruits.sort((a,b) => a.calories - b.calories); // Calories increasing
+    fruits.sort((a,b) => b.calories - a.calories); // Calories decreasing
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp; 
+                                            <b>{fruit.calories}</b></li>);
 
     return(
         <ul>{listItems}</ul>
