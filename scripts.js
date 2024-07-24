97a61c78c5bd6c995815dc28077f5e782e337c35
Buckkeep <neeta.b+github@gmail.com>
2024-07-24 12:58:55 +0100

    Added script to button and some practice JS

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function consoleTime() {
+    console.log("It's Console Time!")
+}
+
+const myChar = {
+    name: "Thunderpaws",
+    fullName: "Thunderpaws McVae",
+    age: 30,
+    occupation: "Dog impersonator",
+    powers: ["laser eyes", "mind control", "drinking water"],
+    isGood: true
+}
+
+console.log(myChar);
+
+console.log("Here are my powers: " + myChar.powers)
+
+console.log("My best power is: " + myChar.powers[0])
+
+
