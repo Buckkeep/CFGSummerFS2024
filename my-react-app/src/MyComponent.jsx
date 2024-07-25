5e5f5daa529b4640f8a087abb17f0f3746a06072
Buckkeep <neeta.b+github@gmail.com>
2024-07-25 13:47:20 +0100

    useState with strings, numbers and boolean. Default values included.

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,7 +2,9 @@ import React, {useState} from "react"
 
 function MyComponent() {
 
-    let [name, setName] = useState();
+    let [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         // name = "Buckkeep";
@@ -10,10 +12,24 @@ function MyComponent() {
         setName("Buckkeep")
     }
 
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
+    }
+
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increase Age</button>
+
+            <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={toggleEmployedStatus}>Toggle Employment Status</button>
         </div>
     );
 
