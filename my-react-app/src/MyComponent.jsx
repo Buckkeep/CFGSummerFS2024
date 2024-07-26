bc7715190b947ce2d666e56371222513a5328ea5
Buckkeep <neeta.b+github@gmail.com>
2024-07-26 13:46:10 +0100

    Updating arrays in state

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,31 +2,33 @@ import React, {useState} from "react";
 
 function MyComponent(){
 
-    const [car, setCar] = useState({year:2005, 
-                                    make:"VW", 
-                                    model:"Golf"})
+    const [foods,setFoods] = useState(["Apple", "Orange", "Banana"])     
+    
+    function handleAddFood() {
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
 
-    function handleYearChange(event) {
-        setCar(prevCar => ({...prevCar, year: event.target.value}));
+        // setFoods([...foods, newFood]) // naive way
+        setFoods(f => [...f, newFood])  // using updater function
+
     }
 
-    function handleMakeChange(event) {
-        setCar(prevCar => ({...prevCar, make: event.target.value}));
+    function handleRemoveFood(index) {
+        setFoods(foods.filter((_, i) => i !== index));
     }
-
-    function handleModelChange(event) {
-        setCar(prevCar => ({...prevCar, model: event.target.value}));        
-    }
-                               
 
     return(
         <div>
-            <p>Your favourite car is: {car.year} {car.make} {car.model}</p>
-            
-            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
-            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
+            <h2>List of Food</h2>
+            <ul>
+                {foods.map((food, index) => 
+                <li key={index} onClick={() => handleRemoveFood(index)}> 
+                    {food}
+                </li>)} 
+            </ul>
 
+            <input type="text" id="foodInput" placeholder="Enter food name" />
+            <button onClick={handleAddFood}>Add Food</button>
         </div>
     );
 }
