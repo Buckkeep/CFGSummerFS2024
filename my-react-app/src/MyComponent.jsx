7fdb2bc6c37a4835af682f09e9a8581c0d9e23f9
Buckkeep <neeta.b+github@gmail.com>
2024-07-29 17:00:41 +0100

    Changing text input fields colour without re-rendering, using useRef

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,20 +2,45 @@ import React, {useState, useEffect, useRef} from 'react'
 
 function MyComponent() {
 
-  const ref = useRef(0);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
 
   useEffect(() => {
-    console.log("Component rendered!")
+    console.log("Component rendered!");
   });
 
-  function handleClick() {
-    ref.current++;
-    console.log(ref.current);
+  function handleClick1() {
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = "yellow"
+    inputRef2.current.style.backgroundColor = ""
+    inputRef3.current.style.backgroundColor = ""
+  }
+
+  function handleClick2() {
+    inputRef2.current.focus();
+    inputRef1.current.style.backgroundColor = ""
+    inputRef2.current.style.backgroundColor = "yellow"
+    inputRef3.current.style.backgroundColor = ""
+  }
+
+  function handleClick3() {
+    inputRef3.current.focus();
+    inputRef1.current.style.backgroundColor = ""
+    inputRef2.current.style.backgroundColor = ""
+    inputRef3.current.style.backgroundColor = "yellow"
   }
     
   return (
     <div>
-      <button onClick={handleClick}>Click Me!</button>
+      <button onClick={handleClick1}>Click Me 1!</button>
+      <input ref={inputRef1}/>
+
+      <button onClick={handleClick2}>Click Me 2!</button>
+      <input ref={inputRef2}/>
+
+      <button onClick={handleClick3}>Click Me 3!</button>
+      <input ref={inputRef3}/>
     </div>
   )
 }
