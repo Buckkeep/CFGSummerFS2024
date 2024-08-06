eb8e3632ef2b29f7c6cc7c4337a35d5be842781f
Buckkeep <neeta.b+github@gmail.com>
2024-08-06 17:00:52 +0100

    Basic button shapes with some individualisation.

---
+++ csspractice/src/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Start() {
+  return (
+    <>
+    <div>Start</div>
+    <button className="btn">Just a button</button>
+    <button className="btn btn-start">Start</button>
+    <button className="btn btn-cuisine">Cuisine</button>
+    <button className="btn btn-budget">Budget</button>
+    <button className="btn btn-location">Location</button>
+    <button className="btn btn-home">Home</button>
+    <button className="btn btn-info">Info</button>
+    </>
+  )
+}
+
+export default Start
