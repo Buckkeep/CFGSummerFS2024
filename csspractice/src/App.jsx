a3df2d6491ede9b3fa97be80c4fd890a04d20b2c
Buckkeep <neeta.b+github@gmail.com>
2024-08-07 09:41:36 +0100

    Removed borders from buttons and added margins

--- csspractice/src/App.jsx
+++ csspractice/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Start from './Start'
+import StarterCSS from './StarterCSS'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Start />
+      <StarterCSS />
     </>
   )
 }
--- csspractice/src/StarterCSS.jsx
+++ csspractice/src/StarterCSS.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-function Start() {
+function StarterCSS() {
   return (
     <>
     <div>Start</div>
     <button className="btn">Just a button</button>
+
     <button className="btn btn-start">Start</button>
+
     <button className="btn btn-cuisine">Cuisine</button>
+  
     <button className="btn btn-budget">Budget</button>
+    
     <button className="btn btn-location">Location</button>
+
     <button className="btn btn-home">Home</button>
+
     <button className="btn btn-info">Info</button>
     </>
   )
 }
 
-export default Start
+export default StarterCSS
