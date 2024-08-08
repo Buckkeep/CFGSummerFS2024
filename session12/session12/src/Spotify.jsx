ffff2eb4d48897d990370e7238d828735f83938a
Buckkeep <neeta.b+github@gmail.com>
2024-08-08 13:18:31 +0100

    Session 12 adding Spotify card from their embed code

---
+++ session12/session12/src/Spotify.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Spotify() {
+  return (
+    <>
+        <iframe style={{borderRadius:"12px"}} 
+        src="https://open.spotify.com/embed/artist/7dGJo4pcD2V6oG8kP0tJRR?utm_source=generator" width="100%" 
+        height="352" 
+        allowFullScreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" />
+    </>
+  )
+}
+
+export default Spotify
